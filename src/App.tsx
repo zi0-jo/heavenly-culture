ce2510ff9d1f2af1fdf0d150919c08f0f488b0a1
jiyoung.jo <jiyoung.jo@worksmobile.com>
2023-01-01 20:46:52 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">마태지파 문화부 인재모집</header>
+    </div>
+  );
+}
+
+export default App;
