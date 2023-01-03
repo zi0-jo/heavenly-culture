93d776a4879a370ac95399797ca1125c52077493
zi0-jo <zi0-jo88@gmail.com>
2023-01-03 11:17:09 +0900

    chore: 지원자격 애니메이션 세부 조정

--- src/content/components/Condition.tsx
+++ src/content/components/Condition.tsx
@@ -20,6 +20,7 @@ export default function Condition({ condition, className }: Props) {
     >
       <Title className="mr-5 w-[200px] py-4">지원자격</Title>
       <ul
+        id="condition-list"
         className={`text-md max-w-[650px] p-4 text-black [&>li]:mb-3 [&>li]:w-full [&>li]:rounded-full [&>li]:bg-yellow-400/80 [&>li]:py-2 [&>li]:px-5`}
       >
         {condition?.map((str, index) => (
@@ -36,17 +37,13 @@ const setAnimation = () => {
       smoothChildTiming: true,
       scrollTrigger: {
         start: 'center bottom',
-        end: 'bottom center',
-        // markers: true,
+        end: 'center center',
+        markers: true,
         scrub: true,
         trigger: '#condition-section',
         scroller: '#content-container',
       },
     })
     .add('first')
-    .fromTo(
-      '#condition-section li',
-      { y: 200, opacity: 0 },
-      { y: 0, opacity: 1 },
-    );
+    .fromTo('#condition-list', { y: 250, opacity: 0 }, { y: 0, opacity: 1 });
 };
